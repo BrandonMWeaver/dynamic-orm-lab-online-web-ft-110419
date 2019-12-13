75ca760f97ce17130f3dd10b4f527556ba1cecf3
Brandon Weaver <a7xncob@gmail.com>
2019-12-13 15:42:41 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -30,7 +30,7 @@ class InteractiveRecord
   end
   
   def col_names_for_insert
-    return self.class.column_names.delete_if()
+    return self.class.column_names.delete_if { |col| col ==
   end
   
 end
